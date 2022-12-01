2c325349df0b093585e13da725285cf465d3451e
Tran Thuan <tranthuan.ttt@gmail.com>
2022-12-01 12:51:52 +0700

    testing

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,22 +3,8 @@ import Banner from 'src/components/Banner';
 import Header from 'src/components/Header';
 import WeatherCard from 'src/components/WeatherCard';
 
-const Home: React.FC = () => {
-  return (
-    <div>
-      <div style={{ width: '100vw', display: 'flex', justifyContent: 'center' }}>
-        <Header />
-      </div>
-      <div style={{ marginBottom: 15 }}>
-        <Banner />
-      </div>
-      <div style={{ width: '100vw', display: 'flex', justifyContent: 'center' }}>
-        <div style={{ flex: 1, maxWidth: 1024, padding: 15 }}>
-          <WeatherCard />
-        </div>
-      </div>
-    </div>
-  );
+const Home = () => {
+  return <div>home pages 123</div>;
 };
 
 Home.propTypes = {};
